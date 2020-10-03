86a4d31a0c345fedb31b0a77df94e84a26e8f4ba
micac <micaela.caro@gmail.com>
2020-10-03 13:48:55 -0300

    Level 3 - Hashing Passwords

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -19,8 +19,6 @@ const userSchema = new mongoose.Schema ({
   email:String,
   password: String
 });
-
-userSchema.plugin(encrypt, { secret:process.env.SECRET, encryptedFields:['password'] }); /*adds encryption package to extend functionality to mongoose schema*/
 
 const User = new mongoose.model("User", userSchema);
 
@@ -39,7 +37,7 @@ app.get("/register", function(req, res){
 app.post("/register", function(req, res){
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
   newUser.save(function(err){
     if(!err){
@@ -53,7 +51,7 @@ app.post("/register", function(req, res){
 
 app.post("/login", function(req, res){
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({email: username}, function(err, foundUser){
     if(!err){
