bfdb03396ffa4d2034fa98a0a1be1c1db1f86187
micac <micaela.caro@gmail.com>
2020-10-03 17:45:57 -0300

    Let users submit secrets

--- app.js
+++ app.js
@@ -33,7 +33,8 @@ mongoose.set("useCreateIndex", true);
 const userSchema = new mongoose.Schema ({
   email:String,
   password: String,
-  googleId: String
+  googleId: String,
+  secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -92,12 +93,43 @@ app.get("/register", function(req, res){
 });
 
 app.get("/secrets", function(req, res){
+    User.find({"secret": {$ne: null}}, function(err,foundUsers){
+      if(err){
+        console.log(err);
+      }
+      else{
+        if(foundUsers){
+          res.render("secrets", {usersWithSecrets: foundUsers});
+        }
+      }
+    })
+});
+
+app.get("/submit", function(req, res){
     if(req.isAuthenticated()){
-      res.render("secrets");
+      res.render("submit");
     }
     else{
       res.redirect("/login");
     }
+});
+
+app.post("/submit", function(req, res){
+  const submittedSecret = req.body.secret;
+
+  User.findById(req.user.id, function(err, foundUser){
+    if(err){
+      console.log(err);
+    }
+    else{
+      if(foundUser){
+        foundUser.secret = submittedSecret;
+        foundUser.save(function(){
+          res.redirect("/secrets");
+        });
+      }
+    }
+  });
 });
 
 app.post("/register", function(req, res){
